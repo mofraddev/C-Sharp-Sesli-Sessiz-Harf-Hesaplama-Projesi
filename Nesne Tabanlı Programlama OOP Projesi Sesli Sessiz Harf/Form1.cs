f30def90eb06f469d3d6584b88908319990e87a0
Barış Zıbahalat Mofrad <mofrad@outlook.com.tr>
2022-03-26 15:23:13 +0300

    Revert "Prokect Finished"
    This reverts commit ae4ba7872640057bc08fdf6baed6504a10ffa6c5.

--- Nesne Tabanlı Programlama OOP Projesi Sesli Sessiz Harf/Form1.cs
+++ Nesne Tabanlı Programlama OOP Projesi Sesli Sessiz Harf/Form1.cs
@@ -16,37 +16,5 @@ namespace Nesne_Tabanlı_Programlama_OOP_Projesi_Sesli_Sessiz_Harf
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            char[] sesli = { 'a', 'e', 'i', 'ı', 'o', 'ö', 'u', 'ü', 'A', 'E', 'İ', 'I', 'O', 'Ö', 'U', 'Ü' };
-            int sessiz_harf = 0;
-            int sesli_harf = 0;
-            int z = 0;
-            string kelime = textBox1.Text;
-            foreach (char harf in kelime)
-            {
-                z = 0;
-                for (int i = 0; i < sesli.Length; i++)
-                {
-                    if (sesli[i] == harf)
-                    {
-                        z++;
-                        sesli_harf++;
-                    }
-                }
-                if (z == 0)
-                {
-                    sessiz_harf++;
-                }
-            }
-            label4.Text = sessiz_harf.ToString();
-            label5.Text = sesli_harf.ToString();
-        }
     }
 }
